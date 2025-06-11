adf1b58891a014b2fcebb2b348689c2fa26499ca
1-WOOPER-1 <v123vladimir@gmail.com>
2025-06-11 23:29:02 +0300

    Initial version of the version quotes app using OOP and classes - choosing a random quote from the array of quotes

---
+++ src/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: "468a8ce3",
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    id: "3d33ae0e",
+    text: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+    author: "Martin Luther King Jr.",
+  },
+  {
+    id: "875ebd1c",
+    text: "Be the change that you wish to see in the world.",
+    author: "Mahatma Gandhi",
+  },
+  {
+    id: "4f095090",
+    text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    id: "543df673",
+    text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    id: "4ca19cb2",
+    text: "The only thing we have to fear is fear itself.",
+    author: "Franklin D. Roosevelt",
+  },
+  {
+    id: "3f6ea685",
+    text: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson",
+  },
+  {
+    id: "97bf12a2",
+    text: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    id: "1323618c",
+    text: "The best time to plant a tree was 20 years ago. The second best time is now.",
+    author: "Chinese Proverb",
+  },
+  {
+    id: "519db185",
+    text: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+];
+
+export default quotes;
