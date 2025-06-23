dfec7e46693ed9f129e0fe90cc791ba64551e9ab
1-WOOPER-1 <v123vladimir@gmail.com>
2025-06-23 22:02:53 +0300

    Creating own quotes API Node.js Express server with resource /quotes/random-single

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: "468a8ce3",
+    text: "Do not wait for leaders; do it alone, person to person.",
+    author: "Mother Teresa",
+  },
+  {
+    id: "3d33ae0e",
+    text: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    id: "875ebd1c",
+    text: "Act as if what you do makes a difference. It does.",
+    author: "William James",
+  },
+  {
+    id: "4f095090",
+    text: "A person who never made a mistake never tried anything new.",
+    author: "Albert Einstein",
+  },
+  {
+    id: "543df673",
+    text: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: "4ca19cb2",
+    text: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+  {
+    id: "3f6ea685",
+    text: "If you tell the truth, you don't have to remember anything.",
+    author: "Mark Twain",
+  },
+  {
+    id: "97bf12a2",
+    text: "Everything you can imagine is real.",
+    author: "Pablo Picasso",
+  },
+  {
+    id: "1323618c",
+    text: "Don’t count the days, make the days count.",
+    author: "Muhammad Ali",
+  },
+  {
+    id: "519db185",
+    text: "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+];
+
+module.exports = quotes;
